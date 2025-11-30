3ca341b36faf04a9cfed6b3652d7ea76b461fe8f
JoseM4rcio <jm4536@outlook.com>
2025-11-30 17:16:51 -0300

    Testando classes de erro

---
+++ src/core/either.spec.ts
@@ -0,0 +1,24 @@
+import { tr } from "@faker-js/faker/.";
+import { Either, left, right } from "./either";
+
+function doSomething(shouldSucceed: boolean): Either<string, number> {
+  if (shouldSucceed) {
+    return right(10);
+  } else {
+    return left("error");
+  }
+}
+
+test("sucess result", () => {
+  const result = doSomething(true);
+
+  expect(result.isRight()).toBe(true);
+  expect(result.isLeft()).toBe(false);
+});
+
+test("error result", () => {
+  const result = doSomething(false);
+
+  expect(result.isLeft()).toBe(true);
+  expect(result.isRight()).toBe(false);
+});
